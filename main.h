e156de343a697eab61725ff385f5a1cd0cd53e59
Milo Amorsolo <mamorsolo@reboundtx.com>
2017-10-26 23:20:33 +0000

    Workin branch

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#define 	CY_PORT 				5432
+#define 	CONTROLS_TIMEOUT		5000
+#define 	KNOB_COUNT 				8
+#define 	KNOB_CHANGE_THRESHOLD 	3
+
+#define		BTN_COUNT	4
+
+#define		BTN_IO4		0
+#define		BTN_IO17	1
+#define		BTN_IO18	2
+#define		BTN_IO27	3
+
+extern bool gbBtnPressed[BTN_COUNT];
+
+#endif
